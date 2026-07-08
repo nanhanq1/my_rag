81282464e0656fbcae6b26de1a177217523ebf14
GaoChengquan <529390053@qq.com>
2026-07-08 16:37:57 +0800

    对接模型

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,6 +1,8 @@
+import os
 from pathlib import Path
 
 from fastapi import FastAPI
+from openai import OpenAI
 from starlette.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 
@@ -12,6 +14,49 @@ app = FastAPI(title="RAG企业知识库", version="1.0.0")
 BASE_DIR = Path(__file__).resolve().parent.parent
 # 前端目录
 WEB_DIR = BASE_DIR / "web"
+
+
+@app.get("/chat")
+def chat(question: str):
+    logger.info(f"用户问题：{question}")
+    client = OpenAI(
+        # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx"
+        api_key="sk-ws-H.EMIMMMD.Ubom.MEUCIDYmaGtoLwamCG1d6JQFqSN5EAzVundhZfApRqwLkhwoAiEA80Dl8lc41yy0vvPpw4B-akbKfq-psHV793IQIgyGMNI",
+        base_url="https://ws-10v53f71kp1ik6yu.cn-beijing.maas.aliyuncs.com/compatible-mode/v1",
+    )
+
+    completion = client.chat.completions.create(
+        # 模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+        model="qwen3.7-plus",
+        messages=[
+            {"role": "system", "content": "我是思途AI助手"},
+            {"role": "user", "content": question},
+        ]
+    )
+    print(completion.model_dump_json())
+    logger.info(f"模型输出：{completion.choices[0].message.content}")
+    return completion.choices[0].message.content
+
+
+@app.get("/chat_stream")
+def chat_stream(question: str):
+    logger.info(f"聊天请求: {question}")
+    client = OpenAI(
+        # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx"
+        api_key="sk-ws-H.EMIMMMD.Ubom.MEUCIDYmaGtoLwamCG1d6JQFqSN5EAzVundhZfApRqwLkhwoAiEA80Dl8lc41yy0vvPpw4B-akbKfq-psHV793IQIgyGMNI",
+        base_url="https://ws-10v53f71kp1ik6yu.cn-beijing.maas.aliyuncs.com/compatible-mode/v1",
+    )
+    completion = client.chat.completions.create(
+        model="qwen3.7-plus",
+        # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+        messages=[{'role': 'system', 'content': '我是思途AI助手'},
+                  {'role': 'user', 'content': question}],
+        stream=True,
+        stream_options={"include_usage": True}
+    )
+    for chunk in completion:
+        if chunk.choices and chunk.choices[0].delta.content:
+            print(chunk.choices[0].delta.content, end="", flush=True)
 
 @app.get("/")
 def index():
